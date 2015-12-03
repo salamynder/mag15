42d39afaa4deb726b8258cae23b0cf46b03d0bde
salamynder <rtbtobi@gmail.com>
2015-12-03 22:15:50 +0100

    beginning eval

---
+++ tests/do_csv2dbtex.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# ~/datatooltk/bin/datatooltk --name shideoveruse --nocsvheader --sep ';' --map-tex-specials --filter Field6 regex ".*overuse.*" --csv eng_chi3.csv -o shideoveruse.dbtex 
+
+~/datatooltk/bin/datatooltk --name tenses --nocsvheader --sep ';' --map-tex-specials --filter Field6 regex ".*tense.*" --filter Field7 regex ".*tense.*" --csv eng_chi3.csv -o tenses.dbtex 
