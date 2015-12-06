ee4bcc781a3cbf2bd7bee6591a26f5a5a10a1b4b
salamynder <rtbtobi@gmail.com>
2015-12-06 21:09:19 +0100

    begin shi-de-eval and table row numbering

--- tests/do_csv2dbtex.sh
+++ tests/do_csv2dbtex.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
+DTOOL=~/datatooltk/bin/datatooltk
 
-~/datatooltk/bin/datatooltk --name shideoveruse --nocsvheader --sep ';' --map-tex-specials --filter Field6 regex ".*overuse.*" --csv eng_chi3.csv -o shideoveruse.dbtex 
+$DTOOL --name shide-needed --nocsvheader --sep ';' --map-tex-specials --filter Field6 regex ".*是-的-necessary.*" --csv eng_chi3.csv -o shide-needed.dbtex 
 
-~/datatooltk/bin/datatooltk --name tenses --nocsvheader --sep ';' --map-tex-specials --filter Field6 regex ".*tense.*" --filter Field7 regex ".*tense.*" --csv eng_chi3.csv -o tenses.dbtex 
+$DTOOL --name shideoveruse --nocsvheader --sep ';' --map-tex-specials --filter Field6 regex ".*是-的-overuse.*" --csv eng_chi3.csv -o shideoveruse.dbtex 
+
+$DTOOL --name tenses --nocsvheader --sep ';' --map-tex-specials --filter Field6 regex ".*tense.*" --filter Field7 regex ".*tense.*" --csv eng_chi3.csv -o tenses.dbtex 
