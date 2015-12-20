82381990acc95e03f51006041b45821593b7bac3
salamynder <rtbtobi@gmail.com>
2015-12-20 17:46:59 +0100

    eval: beginning aspect/tense

--- tests/do_csv2dbtex.sh
+++ tests/do_csv2dbtex.sh
@@ -1,22 +1,37 @@
 #!/bin/sh
 DTOOL=~/datatooltk/bin/datatooltk
 
-$DTOOL --name shide-needed --nocsvheader --sep ';' --map-tex-specials --filter Field6 regex ".*是-的-necessary.*" --csv eng_chi3.csv -o shide-needed.dbtex 
+$DTOOL --name shide-needed --nocsvheader --sep ';' --map-tex-specials \
+       --filter Field6 regex ".*是-的-necessary.*" \
+       --csv eng_chi3.csv -o shide-needed.dbtex 
 
 $DTOOL --name shideoveruseb --nocsvheader --sep ';' --map-tex-specials \
        --filter-and \
        --filter Field6 regex ".*是-的-overuse.*" \
        --filter Field4 eq "B" \
        --csv eng_chi3.csv -o shideoveruse-b.dbtex 
+
 $DTOOL --name shideoveruseg --nocsvheader --sep ';' --map-tex-specials \
        --filter-and \
        --filter Field6 regex ".*是-的-overuse.*" \
        --filter Field4 eq "G" \
+       --sort  -Field2 \
        --csv eng_chi3.csv -o shideoveruse-g.dbtex 
+
 $DTOOL --name shideoveruses --nocsvheader --sep ';' --map-tex-specials \
        --filter-and \
        --filter Field6 regex ".*是-的-overuse.*" \
        --filter Field4 eq "S" \
        --csv eng_chi3.csv -o shideoveruse-s.dbtex 
 
-$DTOOL --name tenses --nocsvheader --sep ';' --map-tex-specials --filter Field6 regex ".*tense.*" --filter Field7 regex ".*tense.*" --csv eng_chi3.csv -o tenses.dbtex 
+
+
+# Zeiten und Konditional
+$DTOOL --name tenses --nocsvheader --sep ';' --map-tex-specials \
+       --filter Field6 regex ".*tense.*" \
+       --sort  -Field6 \
+       --csv eng_chi3.csv -o tenses.dbtex 
+
+$DTOOL --name tenses --nocsvheader --sep ';' --map-tex-specials \
+       --filter Field7 regex ".*conditionalTense.*" \
+       --csv eng_chi3.csv -o condTense.dbtex 
